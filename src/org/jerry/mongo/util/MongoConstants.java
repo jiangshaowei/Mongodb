984fa1a5132f163342ade6ac35e7f4d78a858cd8
jiangshaowei <kangjunlive8@hotmail.com>
2014-12-13 10:15:29 +0800

    add comment in MongoConstants.java

--- src/org/jerry/mongo/util/MongoConstants.java
+++ src/org/jerry/mongo/util/MongoConstants.java
@@ -3,8 +3,11 @@ package org.jerry.mongo.util;
 
 public interface MongoConstants {
 
+	//This is a default server on my
 	public static final String MONGO_HOST ="172.16.0.26";
 	public static final int MONGO_PORT = 27017;
 	public static final String MONGO_DB = "test";
 	public static final String ID_FIELD = "_id";
+	
+
 }
